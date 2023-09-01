16c8466adc167b5c0cebd7c0ddddc8c5b6f48527
Sean Kim <40130483+theskim@users.noreply.github.com>
2023-09-01 01:59:45 -0400

    improved calender feature

--- frontend/src/components/Calender.jsx
+++ frontend/src/components/Calender.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import Calendar from 'react-calendar';
 import { ModalContent, ModalOverlay } from './ModalStyles';
+import { Button } from './ButtonStyles';
 import 'react-calendar/dist/Calendar.css';
 
 const CalendarContainer = styled.div`
@@ -13,11 +14,17 @@ const CalendarContainer = styled.div`
 `;
 
 const EventDot = styled.div`
-  width: 8px;
-  height: 8px;
+  width: 20px;
+  height: 20px;
   border-radius: 50%;
-  background-color: #007bff;
+  background-color: ${props => (props.type === 'interviews' ? '#28a745' : '#007bff')};
   margin-right: 4px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: bold;
+  font-size: 14px;
+  color: #fff;
 `;
 
 const EventLabel = styled.div`
@@ -25,70 +32,159 @@ const EventLabel = styled.div`
   align-items: center;
 `;
 
-export const InterviewModal = ({ interviewData, onClose }) => {
+export const InterviewApplicationModal = ({ interviewData, applicationData, onClose }) => {
+  const [currentPage, setCurrentPage] = useState(0);
+  const [currentDataType, setCurrentDataType] = useState('interview'); // interview or application
+
+  const handleNext = () => {
+    // Calculate the next page, and if it goes beyond the last page, loop back to the first page
+    setCurrentPage((currentPage + 1) % currentData.length);
+  };
+
+  const handlePrev = () => {
+    // Calculate the previous page, and if it goes below the first page, loop to the last page
+    setCurrentPage((currentPage - 1 + currentData.length) % currentData.length);
+  };
+
+  const handleOverlayClick = event => {
+    event.stopPropagation(); // Prevent the click event from reaching the overlay element
+  };
+
+  const handleDataSwitch = dataType => {
+    setCurrentDataType(dataType);
+    setCurrentPage(0);
+  };
+
+  const currentData = currentDataType === 'interview' ? interviewData : applicationData;
+
   return (
     <ModalOverlay onClick={onClose}>
-      <ModalContent>
-        <h2>Interview Details</h2>
-        <p>Company: {interviewData.company}</p>
-        <p>Role: {interviewData.roleName}</p>
-        <p>Date: {interviewData.scheduledInterview}</p>
+      <ModalContent onClick={handleOverlayClick}>
+        <h2>{currentDataType === 'interview' ? 'Interview Details' : 'Application Details'}</h2>
+        {currentData && currentData.length > 0 ? (
+          <>
+            <div>
+              <p>Company: {currentData[currentPage].company}</p>
+              <p>Role: {currentData[currentPage].roleName}</p>
+              <p>Location: {currentData[currentPage].location}</p>
+              {currentDataType === 'interview' ? (
+                <p>Interview Date: {currentData[currentPage].scheduledInterview}</p>
+              ) : (
+                <p>Date Applied: {currentData[currentPage].dateApplied}</p>
+              )}
+            </div>
+            <div>
+              <h3>{currentPage + 1}/{currentData.length}</h3>
+              <Button className='gray' type="button" onClick={handlePrev}>Previous</Button>
+              <Button className='gray' type="button" onClick={handleNext}>Next</Button>
+              <Button
+                className='blue'
+                type="button"
+                onClick={() =>
+                  handleDataSwitch(
+                    currentDataType === 'application' ? 'interview' : 'application'
+                  )
+                }
+              >
+                {currentDataType === 'interview' ? 'Switch to Applications' : 'Switch to Interviews'}
+              </Button>
+            </div>
+          </>
+        ) : (
+          <div>
+            <p>No {currentDataType === 'interview' ? 'interview' : 'application'} on this day.</p>
+            <Button
+                className='blue'
+                type="button"
+                onClick={() =>
+                  handleDataSwitch(
+                    currentDataType === 'application' ? 'interview' : 'application'
+                  )
+                }
+              >
+                {currentDataType === 'interview' ? 'Switch to Applications' : 'Switch to Interviews'}
+            </Button>
+          </div>
+        )}
       </ModalContent>
     </ModalOverlay>
   );
 };
 
 const TrackCalender = ({ data, selectedDate, handleDateChange }) => {
-    const [showInterviewModal, setShowInterviewModal] = useState(false);
-    const [selectedInterviewData, setSelectedInterviewData] = useState(null);
-  
-    const formattedInterviewDates = data
-      .filter(item => item.state === 'Interview')
-      .map(item => new Date(item.scheduledInterview).toDateString());
+  const [showInterviewModal, setShowInterviewModal] = useState(false);
+  const [selectedInterviewData, setSelectedInterviewData] = useState(null);
+  const [selectedApplicationData, setSelectedApplicationData] = useState(null);
 
-    const formattedApplicationDates = data
-      .map(item => new Date(item.dateApplied).toDateString());
-  
-    const tileContent = ({ date }) => {
-        const formattedDate = date.toDateString(); 
-
-        const isInterviewDate = formattedInterviewDates.includes(formattedDate);
-        const isApplicationDate = formattedApplicationDates.includes(formattedDate);
-
-        if (isInterviewDate || isApplicationDate) {
-            return (
-                <EventLabel>
-                    {isInterviewDate && <EventDot />}
-                    {isApplicationDate && <EventDot />}
-                </EventLabel>
-            );
-        }
-    };
-  
-    const handleTileClick = date => {
-      const formattedDate = date.toDateString();
-      const interviewData = data.find(item => item.state === 'Interview' && new Date(item.scheduledInterview).toDateString() === formattedDate);
-  
-      if (interviewData) {
-        setSelectedInterviewData(interviewData);
-        setShowInterviewModal(true);
-      }
-    };
-  
-    const closeModal = () => {
-      setShowInterviewModal(false);
-      setSelectedInterviewData(null);
-    };
-  
-    return (
-      <CalendarContainer>
-        <h2>Interview and Application Calendar</h2>
-        <Calendar onChange={handleDateChange} value={selectedDate} tileContent={tileContent} onClickDay={handleTileClick} />
-        {showInterviewModal && (
-          <InterviewModal interviewData={selectedInterviewData} onClose={closeModal} />
-        )}
-      </CalendarContainer>
+  const tileContent = ({ date }) => {
+    const formattedDate = date.toDateString(); // Define formattedDate here
+    
+    const interviewsOnSelectedDate = data.filter(
+      item =>
+        item.status === 'Interview' &&
+        new Date(item.scheduledInterview).toDateString() === formattedDate
     );
-};
   
+    const applicationsOnSelectedDate = data.filter(
+      item =>
+        new Date(item.dateApplied).toDateString() === formattedDate
+    );
+
+    return (
+      <EventLabel>
+        {interviewsOnSelectedDate && (
+          <EventDot type="interviews">{interviewsOnSelectedDate.length}</EventDot>
+        )}
+        {applicationsOnSelectedDate && (
+          <EventDot type="applications">{applicationsOnSelectedDate.length}</EventDot>
+        )}
+      </EventLabel>
+    );
+  };
+    
+  const handleTileClick = date => {
+    const formattedDate = date.toDateString(); // Define formattedDate here
+
+    const interviewsOnSelectedDate = data.filter(
+      item =>
+        item.status === 'Interview' &&
+        new Date(item.scheduledInterview).toDateString() === formattedDate
+    );
+  
+    const applicationsOnSelectedDate = data.filter(
+      item =>
+        new Date(item.dateApplied).toDateString() === formattedDate
+    );
+
+    if (interviewsOnSelectedDate && interviewsOnSelectedDate.length > 0){
+      setSelectedInterviewData(interviewsOnSelectedDate);
+      setShowInterviewModal(true);
+    }
+
+    if (applicationsOnSelectedDate && applicationsOnSelectedDate.length > 0){
+      setSelectedApplicationData(applicationsOnSelectedDate);
+      setShowInterviewModal(true);
+    }
+  };
+
+  const closeModal = () => {
+    setShowInterviewModal(false);
+    setSelectedInterviewData(null);
+  };
+
+  return (
+    <CalendarContainer>
+      <h2>Interview and Application Calendar</h2>
+      <Calendar onChange={handleDateChange} value={selectedDate} tileContent={tileContent} onClickDay={handleTileClick} />
+      {showInterviewModal && (
+        <InterviewApplicationModal 
+          interviewData={selectedInterviewData} 
+          applicationData={selectedApplicationData} 
+          onClose={closeModal} 
+        />
+      )}
+    </CalendarContainer>
+  );
+};
+
 export default TrackCalender;

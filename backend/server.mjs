6a6b2d436e756f0ee9be092e2526f067bebaf6a8
michaelzixizhou <106829824+michaelzixizhou@users.noreply.github.com>
2023-09-01 21:35:55 -0400

    Removed https

--- backend/server.mjs
+++ backend/server.mjs
@@ -8,7 +8,6 @@ import passport from "passport";
 import session from "express-session";
 import initializePassport from "./auth.mjs";
 import csrf from "csurf";
-import createError from "http-errors";
 import cookieParser from "cookie-parser";
 
 const PORT = process.env.PORT || 5050;
@@ -46,10 +45,7 @@ app.use(function(req, res, next) {
 
 app.use("/record", records);
 app.use("/user", users);
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
     console.log(`http://localhost:${PORT}/`)
